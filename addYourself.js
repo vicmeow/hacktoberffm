3b5b1c7ea6921ca490f3b94ee74182abf3580d3b
vicbergquist <victoria.bergquist@gmail.com>
2019-10-17 19:35:16 +0200

    add user list

---
+++ addYourself.js
@@ -0,0 +1,6 @@
+// To get started with Hacktoberfest Frankfurt
+// and to add yourself to our Leaderboard
+// please add your GitHub username below
+module.exports = {
+  list: ["rdmueller", "vicbergquist"]
+}
